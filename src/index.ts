cc59934008c675c6ceaeeafa7f0baafc4d181d91
SirYusluv <olanrewajuyusuf379@gmail.com>
2023-06-25 08:20:10 +0100

    file filter and basic config

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,49 @@ import express, {
   Request,
   Response,
 } from "express";
+import path from "path";
+import fs from "fs";
+import multer from "multer";
 import mongoose from "mongoose";
+import { v4 as uuid } from "uuid";
 import * as dotenv from "dotenv";
+
+const PUBLIC_DIR = path.join(__dirname, "..", "public");
+const ALLOWED_IMAGES = ["image/jpeg", "image/png"];
+const FILE_SIZE_LIMIT = 1024 * 200; // 200kb
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 const app = express();
 
+if (!fs.existsSync(PUBLIC_DIR)) fs.mkdirSync(PUBLIC_DIR);
+
+const storage = multer.diskStorage({
+  destination: function (_, _1, cb) {
+    cb(null, PUBLIC_DIR);
+  },
+
+  filename: function (_, file, cb) {
+    const fileExt = path.extname(file.originalname);
+    cb(null, `${file.originalname}${uuid()}${fileExt}`);
+  },
+});
+
+const imageFilter = function (
+  _: Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback
+) {
+  if (ALLOWED_IMAGES.includes(file.mimetype)) cb(null, true);
+  else cb(new Error("provided file not supported"));
+};
+
+const upload = multer({
+  storage,
+  fileFilter: imageFilter,
+});
+
 app.use(express.json());
+app.use(express.static(PUBLIC_DIR));
 
 const errorHandler: ErrorRequestHandler = function (
   err: any,
