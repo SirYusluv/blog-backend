ea768bf1af5dfdf836b1beea948674554b7965e7
SirYusluv <olanrewajuyusuf379@gmail.com>
2023-06-25 09:26:10 +0100

    implemented update functionality

--- src/blog.router.ts
+++ src/blog.router.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Response, Request } from "express";
-import { Blog } from "./blog.schema";
+import { Blog, BlogType } from "./blog.schema";
 
 export async function createBlog(
   req: Request,
@@ -26,12 +26,47 @@ export async function createBlog(
   }
 }
 
+// only title and detail can be updted
 export async function updateBlog(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
   try {
+    const _id = req.params.blogId;
+    if (!_id)
+      return res
+        .status(400)
+        .json({ message: "Blog id must be provided", status: 400 });
+
+    const { title, detail } = req.body;
+
+    // either title or datail must be provided
+    if (!title && !detail)
+      return res.status(400).json({
+        message: "Either title or datail must be provided",
+        status: 400,
+      });
+
+    let blog: BlogType | null = null;
+    if (title && detail)
+      blog = await Blog.findByIdAndUpdate(_id, { title, detail }).exec();
+    if (!blog && title)
+      blog = await Blog.findByIdAndUpdate(_id, { title }).exec();
+    if (!blog && detail)
+      blog = await Blog.findByIdAndUpdate(_id, { detail }).exec();
+
+    if (!blog)
+      return res.status(200).json({
+        message: "Blog Id provided not found",
+        status: 200,
+      });
+
+    res.status(200).json({
+      message: "Blog successfully updated",
+      status: 200,
+      blog: blog.toObject(),
+    });
   } catch (err: any) {
     console.error(err);
     next(err);
